764a971eda1e093e810ba6d326a415285e2856bc
Kyle Harris <koderkow@gmail.com>
2018-06-10 02:28:54 -0400

    added twitter, some touch ups, more attempts at code folding.. :)

--- static/js/codefolding.js
+++ static/js/codefolding.js
@@ -16,7 +16,7 @@ window.initializeCodeFolding = function(show) {
   var currentIndex = 1;
 
   // select all R code blocks
-  var rCodeBlocks = $('pre.sourceCode, pre.r, pre.python, pre.bash, pre.sql, pre.cpp, pre.stan, pre.js');
+  var rCodeBlocks = $('pre.sourceCode, pre.r, pre.python, pre.bash, pre.sql, pre.cpp, pre.stan');
   rCodeBlocks.each(function() {
 
     // create a collapsable div to wrap the code in
